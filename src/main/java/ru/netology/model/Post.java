77ec1d2c9515d35578d30f567d67da3adb655645
Роман <rezample@gmail.com>
2022-07-17 22:28:06 +0400

    Initial commit

---
+++ src/main/java/ru/netology/model/Post.java
@@ -0,0 +1,32 @@
+package ru.netology.model;
+
+public class Post {
+  private long id;
+  private String content;
+
+  public Post() {
+  }
+
+  public Post(long id, String content) {
+    this.id = id;
+    this.content = content;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public Post setId(long id) {
+    this.id = id;
+    return this;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public Post setContent(String content) {
+    this.content = content;
+    return this;
+  }
+}
---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,38 @@
+package ru.netology.repository;
+
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+// Stub
+public class PostRepository {
+  private ConcurrentHashMap<Long, Post> posts = new ConcurrentHashMap<>();
+
+  public List<Post> all() {
+    return posts.entrySet().stream().map(Map.Entry::getValue).collect(Collectors.toList());
+  }
+
+  public Optional<Post> getById(long id) {
+    return Optional.empty();
+  }
+
+  public Post save(Post post) throws NotFoundException {
+    if (posts.containsKey(post.getId())) {
+      posts.put(post.getId(), post);
+    } else if (post.getId() == 0) {
+      posts.putIfAbsent(Integer.valueOf(posts.size() + 1).longValue(), post.setId(posts.size() + 1));
+    } else {
+      throw new NotFoundException();
+    }
+    return post;
+  }
+
+  public void removeById(long id) {
+  }
+}
